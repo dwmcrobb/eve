7df18dd40707147a683558da5ee5ce934e6e0700
dwm <dwm@mcplex.net>
2026-07-22 14:02:40 -0400

    Still tweaking for native packaging.

--- cmake/config/eve-install.cmake
+++ cmake/config/eve-install.cmake
@@ -3,13 +3,24 @@
 ##  Copyright : EVE Project Contributors
 ##  SPDX-License-Identifier: BSL-1.0
 ##==================================================================================================
+cmake_host_system_information(RESULT os_name QUERY OS_NAME)
+if ("${os_name}" STREQUAL "Darwin")
+  if (EXISTS "/opt/local/bin/cmake")
+    set(CMAKE_INSTALL_PREFIX "/opt/local")
+  endif()
+endif()
+
 include(GNUInstallDirs)
 include(CMakePackageConfigHelpers)
 
 set(MAIN_DEST     "${CMAKE_INSTALL_LIBDIR}/eve")
 set(INSTALL_DEST  "${CMAKE_INSTALL_INCLUDEDIR}")
 set(DOC_DEST      "${CMAKE_INSTALL_DOCDIR}")
-
+set(CMAKE_DEST    "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
+    
+message(CMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}")
+message(CMAKE_MODULE_PATH="${CMAKE_MODULE_PATH}")
+    
 ## =================================================================================================
 ## Exporting target for external use
 ## =================================================================================================
@@ -31,19 +42,19 @@ write_basic_package_version_file( "${CMAKE_CURRENT_BINARY_DIR}/eve-config-versio
 ## =================================================================================================
 ## Install target with versioned folder
 ## =================================================================================================
-set(CONFIG_INSTALL_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/eve")
-if (EXISTS "/usr/lib/x86_64-linux-gnu/cmake")
-  set(CONFIG_INSTALL_DIR "/usr/lib/x86_64-linux-gnu/cmake/eve")
-elseif (EXISTS "/usr/lib/aarch64-linux-gnu/cmake")
-  set(CONFIG_INSTALL_DIR "/usr/lib/aarch64-linux-gnu/cmake/eve")
-elseif (EXISTS "/opt/local/lib/cmake")
-  set(CONFIG_INSTALL_DIR "/opt/local/lib/cmake/eve")
-endif()
+# set(CONFIG_INSTALL_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/eve")
+# if (EXISTS "/usr/lib/x86_64-linux-gnu/cmake")
+#   set(CONFIG_INSTALL_DIR "/usr/lib/x86_64-linux-gnu/cmake/eve")
+# elseif (EXISTS "/usr/lib/aarch64-linux-gnu/cmake")
+#   set(CONFIG_INSTALL_DIR "/usr/lib/aarch64-linux-gnu/cmake/eve")
+# elseif (EXISTS "/opt/local/lib/cmake")
+#   set(CONFIG_INSTALL_DIR "/opt/local/lib/cmake/eve")
+# endif()
       
 install(TARGETS   eve_lib EXPORT eve-targets                            DESTINATION "${CONFIG_INSTALL_DIR}" COMPONENT EVEpkg   )
-install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/eve                     DESTINATION "${INSTALL_DEST}" COMPONENT EVEpkg )
-install(FILES     ${PROJECT_SOURCE_DIR}/cmake/eve-config.cmake          DESTINATION "${CONFIG_INSTALL_DIR}" COMPONENT EVEpkg   )
-install(FILES     ${CMAKE_CURRENT_BINARY_DIR}/eve-config-version.cmake  DESTINATION "${CONFIG_INSTALL_DIR}" COMPONENT EVEpkg   )
-install(FILES     ${PROJECT_SOURCE_DIR}/cmake/eve-multiarch.cmake       DESTINATION "${CONFIG_INSTALL_DIR}" COMPONENT EVEpkg   )
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/eve                     DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}" COMPONENT EVEpkg )
+install(FILES     ${PROJECT_SOURCE_DIR}/cmake/eve-config.cmake          DESTINATION "${CMAKE_DEST}" COMPONENT EVEpkg   )
+install(FILES     ${CMAKE_CURRENT_BINARY_DIR}/eve-config-version.cmake  DESTINATION "${CMAKE_DEST}" COMPONENT EVEpkg   )
+install(FILES     ${PROJECT_SOURCE_DIR}/cmake/eve-multiarch.cmake       DESTINATION "${CMAKE_DEST}" COMPONENT EVEpkg   )
 install(FILES     ${PROJECT_SOURCE_DIR}/LICENSE.md                      DESTINATION "${DOC_DEST}" COMPONENT EVEpkg    )
-install(EXPORT    eve-targets NAMESPACE "eve::"                         DESTINATION "${CONFIG_INSTALL_DIR}" COMPONENT EVEpkg   )
+install(EXPORT    eve-targets NAMESPACE "eve::"                         DESTINATION "${CMAKE_DEST}" COMPONENT EVEpkg   )
